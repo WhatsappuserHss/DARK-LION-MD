1cbe874cb42ee26ab03e2ac86478417e9c76520e
WhatsappuserHss <Hashantechinfo@gmail.com>
2025-01-01 20:42:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "yqhHQJCT#dcIh3qg9T_0XjSa7jr0wUut2BNSp4y1_Y2Ndmi-nssg",
+    SESSION_ID: process.env.SESSION_ID || "rjZElbYB#uT3IUYRaviFAwWx2r4r0mKOfIOWWxzZvCH-o8MQ3HnA",
     CAPTION: process.env.CAPTION || "*POWERED BY REDLION MD*",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
